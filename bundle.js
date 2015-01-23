602bd5aecfd393da423044517bd45f633d8d0caa
Russel Simmons <symstym@mac.com>
2015-01-23 12:14:49 -0800

    more work on importer

--- bundle.js
+++ bundle.js
@@ -8,6 +8,7 @@ function importGrammar(grammarStr) {
 
   var warnings = [];
 
+  var shapeNameRules = {}; // map from shape name to list of rules, each rule an object
   var startShapeName;
 
   // transform parse result into internal representation
@@ -16,7 +17,22 @@ function importGrammar(grammarStr) {
     var thing = parseResult[i];
 
     if (thing.type === 'shaperule') {
-      // TODO
+      ruleObj = {
+        weight: thing.weight,
+        prims: [],
+        nonprims: [],
+      }
+
+      // TODO: fill prims and nonprims from thing.replacements.
+      // TODO: instead of .shape properties use .shapeName for now, will resolve later
+
+      // create list of rules for shape name if none yet
+      if (!shapeNameRules.hasOwnProperty(thing.name)) {
+        shapeNameRules[thing.name] = [];
+      }
+
+      // add rule to list
+      shapeNameRules[thing.name].push(ruleObj);
     } else if (thing.type === 'startshape') {
       if (startShapeName) {
         warnings.push('Found multiple startshape directives, using first one');
@@ -26,13 +42,33 @@ function importGrammar(grammarStr) {
     } else if (thing.type === 'size') {
       // TODO
     } else {
-      // TODO: throw error
+      // TODO: throw better error
+      throw "Unrecognized top-level thing";
     }
   }
 
   if (!startShapeName) {
-    // TODO: throw error
+    // TODO: throw better error
+    throw "No startshape directive found";
   }
+
+  // resolve name refs to make final grammar obj
+  // TODO: I think we can just modify shapeNameRules in-place, resolving name refs
+
+  if (!shapeNameRules.hasOwnProperty(startShapeName)) {
+    // TODO: throw better error
+    throw "Name specified by startshape directive not found";
+  }
+
+  var grammar = {
+    startShape: shapeNameRules[startShapeName],
+    // NOTE: we don't actually need to include map/list of shapes here since they are reachable via startShape
+  };
+
+  return {
+    grammar: grammar,
+    warnings: warnings,
+  };
 }
 
 module.exports = {
